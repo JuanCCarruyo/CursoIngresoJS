cc1cdb2c291d575cd6d34ba42a98f4499494b01b
JuanCCarruyo <juanccarruyo@gmail.com>
2020-08-07 23:42:49 -0300

    parc 2020 ej 1

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,38 @@
 function mostrar()
 {
+//	al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
+//  validar que sea numero y que sea mayor a 1
+
+	let num;
+	let contdiv=0;
+	let prim;
+
+	do{
+
+		num = parseInt(prompt("Ingrese un numero mayor a 1"));
+
+	}while(num <= 1 || isNaN(num));
 
 
-	alert("ok");
+	for (let i=1; i<=num; i++){   	
+
+		if (num%i==0){
+			contdiv++;
+		}
+
+	}
+
+	if (contdiv > 2){
+		prim = "El numero "+num+" NO es primo.";
+		
+	}
+	else {
+		prim = "El numero "+num+" es primo.";
+		
+	}
+	
+	alert(prim);
+
+
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,112 @@
 
 function mostrar()
 {
-	alert("uno");
+
+	/*
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+   el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+   el precio (validar entre 100 y 300),
+   la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+   la Marca y el fabricante.
+
+   Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total
+	*/
+
+	let tipo;
+	let precio=0;
+	let cant=0;
+	let marca;
+	let fabr;
+
+	let flag=0;
+	let precmin=0;
+	let cantmin=0;
+	let fabrmin="";
+
+	let canttotalalcohol=0;
+	let canttotaljabon=0;
+	let canttotalbarbijo=0;
+
+	let masunidades=0;
+
+	let a=0;
+	let b=0;
+	let j=0;
+
+	for (let i=0; i<5; i++){
+
+		do{
+			tipo = prompt("Ingresa el tipo de producto");
+		}while (!(tipo == "barbijo" || tipo == "jabón" || tipo == "jabon" || tipo == "alcohol"));
+
+		do{
+			precio = parseInt(prompt("Ingresa el precio del producto"));
+		}while (precio < 100 || precio > 300);
+
+		do{
+			cant = parseInt(prompt("Ingresa la cantidad de unidades"));
+		}while (cant < 1 || cant > 1000);
+
+		marca = prompt("Ingresa la marca del producto");
+
+		fabr = prompt("Ingresa el fabricante del producto");
+
+
+		if (tipo == "alcohol"){
+
+			canttotalalcohol = canttotalalcohol + cant;   // canttotalalcohol es un acumulador
+			a++;                                          // a es un contador
+
+			if (flag == 0 || precmin > precio){
+				precmin = precio;
+				cantmin = cant;
+				fabrmin = fabr;
+				flag=1;
+			}
+
+		}
+
+		if (tipo == "jabon" || tipo == "jabón"){
+			
+			canttotaljabon = canttotaljabon + cant;
+			j++;
+
+		}
+
+		if (tipo == "barbijo"){
+
+			canttotalbarbijo = canttotalbarbijo + cant;
+			b++;
+
+		}
+
+	} 
+
+	if (canttotalalcohol > canttotalbarbijo && canttotalalcohol > canttotaljabon){
+		masunidades = canttotalalcohol;
+		mascont = a;
+	}
+	else if (canttotaljabon > canttotalalcohol && canttotaljabon > canttotalbarbijo){
+		masunidades = canttotaljabon;
+		mascont = j;
+	}
+	else {
+		masunidades = canttotalbarbijo;
+		mascont = b;
+	}
+
+	document.write("a) Del más barato de los alcohol, la cantidad de unidades y el fabricante"+"<br>");
+	document.write("Es el fabricante "+fabrmin+" con "+cantmin+" unidades."+"<br>"+"<br>");
+
+	document.write("b) Del tipo con mas unidades, el promedio por compra"+"<br>");
+	document.write("Es "+(masunidades/mascont)+"<br>"+"<br>");
+
+	document.write("c) Cuántas unidades de jabones hay en total"+"<br>");
+	document.write("Hay "+canttotaljabon+" unidades de jabon.");
+
+
 }
